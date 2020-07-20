086001b765fc56fa2509c98fb0643a995eb2bae2
Eric Sherman <ericandrewsherman@gmail.com>
2020-07-20 17:13:04 -0500

    housing, shelter

---
+++ assets/vizList.js
@@ -0,0 +1,82 @@
+const vizList = [
+    {
+      "section": 1,
+      "section_title": "Newly Homeless",
+      "layer": 1,
+      "visualization_description": "Change in the number of people who are newly homeless",
+      "link": "https://public.tableau.com/profile/sarah.rodriguez#!/vizhome/Changeinthenumberofpeoplewhoarenewlyhomeless/Sheet1?publish=yes",
+      "url": "https://public.tableau.com/views/Changeinthenumberofpeoplewhoarenewlyhomeless/Sheet1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    },
+    {
+      "section": 1,
+      "section_title": "Newly Homeless",
+      "layer": 2,
+      "visualization_description": "Break down of the demographics of people who are newly homeless",
+      "link": "https://public.tableau.com/profile/sarah.rodriguez#!/vizhome/Breakdownofthedemographicsofpeoplewhoarenewlyhomeless/Sheet2?publish=yes",
+      "url": "https://public.tableau.com/views/Breakdownofthedemographicsofpeoplewhoarenewlyhomeless/Sheet2?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    },
+    {
+      "section": 2,
+      "section_title": "Shelter",
+      "layer": 1,
+      "visualization_description": "Change in the total number of beds for shelter",
+      "link": "https://public.tableau.com/profile/sarah.rodriguez#!/vizhome/Changeinthetotalnumberofbedsusedtoshelterpeople/HousingandShelter2",
+      "url": "https://public.tableau.com/views/Changeinthetotalnumberofbedsusedtoshelterpeople/HousingandShelter2?:language=en&:display_count=y&:origin=viz_share_link"
+    },
+    {
+      "section": 2,
+      "section_title": "Shelter",
+      "layer": 2,
+      "visualization_description": "Change in the total number of people who receive shelter",
+      "link": "https://public.tableau.com/profile/sarah.rodriguez#!/vizhome/Changeinthetotalnumberofpeoplewhoreceiveshelter/Sheet1?publish=yes",
+      "url": "https://public.tableau.com/views/Changeinthetotalnumberofpeoplewhoreceiveshelter/Sheet1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    },
+    {
+      "section": 2,
+      "section_title": "Shelter",
+      "layer": 3,
+      "visualization_description": "Comparison of the demographics of people in shelter and people who live in Travis County ",
+      "link": "https://public.tableau.com/profile/sarah.rodriguez#!/vizhome/ComparisonofthedemographicsofpeopleinshelterandpeoplewholiveinTravisCounty/Total-Shelter?publish=yes",
+      "url": "https://public.tableau.com/views/ComparisonofthedemographicsofpeopleinshelterandpeoplewholiveinTravisCounty/Total-Shelter?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    },
+    {
+      "section": 3,
+      "section_title": "Housing",
+      "layer": 1,
+      "visualization_description": "Change in the total number of beds for housing",
+      "link": "https://public.tableau.com/profile/sarah.rodriguez#!/vizhome/Changeinthetotalnumberofbedsusedtohousepeople/HousingandShelter3?publish=yes",
+      "url": "https://public.tableau.com/views/Changeinthetotalnumberofbedsusedtohousepeople/HousingandShelter3?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    },
+    {
+      "section": 3,
+      "section_title": "Housing",
+      "layer": 2,
+      "visualization_description": "Change in the total number of people who exit to homelessness to housing",
+      "link": "https://public.tableau.com/profile/sarah.rodriguez#!/vizhome/Exits_15930134501040/Exits?publish=yes",
+      "url": "https://public.tableau.com/views/Exits_15930134501040/Exits?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    },
+    {
+      "section": 3,
+      "section_title": "Housing",
+      "layer": 3,
+      "visualization_description": "Comparison of the demographics of people who are sheltered and exit to housing",
+      "link": "https://public.tableau.com/profile/sarah.rodriguez#!/vizhome/Comparisonofthedemographicsofpeoplewhoareshelteredandexittohousing/Total-Housed?publish=yes",
+      "url": "https://public.tableau.com/views/Comparisonofthedemographicsofpeoplewhoareshelteredandexittohousing/Total-Housed?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    },
+    {
+      "section": 4,
+      "section_title": "Returns to Homelessness",
+      "layer": 1,
+      "visualization_description": "Change in the total number of people who return to homelessness",
+      "link": "https://public.tableau.com/profile/sarah.rodriguez#!/vizhome/ReturnstoHomelessness_15919382098890/Returns-total?publish=yes",
+      "url": "https://public.tableau.com/views/ReturnstoHomelessness_15919382098890/Returns-total?:language=en&:display_count=y&publish=yes&:origin=viz_share_link"
+    },
+    {
+      "section": 4,
+      "section_title": "Returns to Homelessness",
+      "layer": 2,
+      "visualization_description": "Comparison of the demographics of people who exit to housing and return to homelessness",
+      "link": "https://public.tableau.com/profile/sarah.rodriguez#!/vizhome/Comparisonofthedemographicsofpeoplewhoexittohousingandreturntohomelessness/Total-Returns",
+      "url": "https://public.tableau.com/views/Comparisonofthedemographicsofpeoplewhoexittohousingandreturntohomelessness/Total-Returns?:language=en&:display_count=y&:origin=viz_share_link"
+    }
+   ];
