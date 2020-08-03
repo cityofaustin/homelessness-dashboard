f2de95ba42ce2fb0129254a8c0351f40cc3cbb4a
Eric Sherman <ericandrewsherman@gmail.com>
2020-08-03 11:50:43 -0500

    nix fancy markdown, update disclaimer copy from draft doc

--- assets/disclaimer.js
+++ assets/disclaimer.js
@@ -1,15 +1,11 @@
-const dataDisclaimer = `## Data disclaimer
-
+const dataDisclaimer = `<h1>Data disclaimer</h1>
+<p>
 Austin's Continuum of Care (CoC) has gathered the data presented in this dashboard. A Continuum of Care is a regional group of non-profit organizations that provide services to the homeless community. 
-
+</p>
+<p>
 While there is a wide range of organizations that serve Austin's homeless community, this dashboard only represents data gathered by certain members of the Continuum of Care. This dashboard does not include data from organizations whose collection methods are incompatible with the larger group. This dashboard also does not include data gathered outside the Continuum of Care. 
-
+</p>
+<p>
 There are people experiencing homelessness who choose not to seek services from the CoC. This data does not include those individuals.
-
-## Data discrepancies
-
-Because of the nature of homelessness case-work, data entry is incomplete, imperfect, and sometimes data changes after it is reported. The data featured in this dashboard is a snapshot of the issue of homelessness in Austin, but it is not comprehensive.
-
-The Continuum of Care is in the process of analyzing 2019 data, and there are some metrics that still need to be updated.
-
-Learn more about homelessness terminology and how this data is collected. [link]`;
+</p>
+<p>`;
