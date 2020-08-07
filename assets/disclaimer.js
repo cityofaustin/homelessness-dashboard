0d509c4bba2cb1908c3f093440aa104aca0265d0
ablangworthy27 <ablangworthy27@gmail.com>
2020-08-07 16:10:18 -0500

    Update disclaimer.js

--- assets/disclaimer.js
+++ assets/disclaimer.js
@@ -1,6 +1,6 @@
-const dataDisclaimer = `<h1>Data disclaimer</h1>
+const dataDisclaimer = `<h1>Data Disclaimer</h1>
 <p>
-Austin's Continuum of Care (CoC) has gathered the data presented in this dashboard. A Continuum of Care is a regional group of non-profit organizations that provide services to the homeless community. 
+Austin and Travis County’s Continuum of Care (CoC) has gathered the data presented in this dashboard. A Continuum of Care is a regional group of non-profit organizations that provide services to the population experiencing homelessness.
 </p>
 <p>
 While there is a wide range of organizations that serve Austin's homeless community, this dashboard only represents data gathered by certain members of the Continuum of Care. This dashboard does not include data from organizations whose collection methods are incompatible with the larger group. This dashboard also does not include data gathered outside the Continuum of Care. 
@@ -8,4 +8,14 @@ While there is a wide range of organizations that serve Austin's homeless commun
 <p>
 There are people experiencing homelessness who choose not to seek services from the CoC. This data does not include those individuals.
 </p>
-<p>`;
+<h1>Data Discrepancies
+</h1>
+<p>
+Because of the nature of homelessness case work, data entry is incomplete, imperfect, and sometimes data changes after it is reported. The data featured in this dashboard is a snapshot of the issue of homelessness in Austin, but it is not comprehensive.
+</p>
+<p>
+The Continuum of Care is in the process of analyzing 2019 data, and there are some metrics that still need to be updated. Data visualized in the dashboard will change as more current data becomes available.
+</p>
+<p>
+<a href="http://austintexas.gov/department/homelessness-glossary">Learn more about homelessness terminology and how this data is collected</a>. 
+</p>`;
